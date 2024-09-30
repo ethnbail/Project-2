5fb50ed6ec34390e6b8b366a0aa753b1d52105f0
ethnbail <findeab@gmail.com>
2024-09-29 21:06:56 -0700

    9.22-6.cpp

--- 9.22-6.cpp
+++ 9.22-6.cpp
@@ -7,10 +7,10 @@
  using namespace std;
 
  class  CashRegister{
-    
+
     private:
         vector<double> prices; // stores the prices of each item
-    
+
     public:
         void clear(){
             prices.clear();
@@ -27,11 +27,11 @@
             }
             return total;
         }
-        
+
         int get_count() const{ // get count of the items
             return prices.size();
         }
-        
+
         void display_all() const{ // display all the items prices
             cout << fixed << setprecision(2);
             cout << "Prices of all the items currently in the sale: " << endl; 
@@ -44,24 +44,26 @@
   {
 
     CashRegister register1;
-    
+
     register1.clear();
-    register1.add_item(2.99);
-    register1.add_item(3.49);
+    register1.add_item(22.99);
+    register1.add_item(13.49);
     register1.add_item(5.29);
-    register1.add_item(1.98);
-    register1.add_item(4.49);
+    register1.add_item(21.98);
+    register1.add_item(14.49);
     register1.add_item(5.99);
     register1.add_item(14.60);    
+      
+    register1.display_all();
+      
 
     cout << fixed << setprecision(2); // since its money it should display both decismal places
-    cout << "Total: $" << register1.get_total() << endl;
-    cout << "Number of items: " << register1.get_count() << endl;
+      cout << "Number of items: " << register1.get_count() << endl;
 
-    register1.display_all();
+    cout << "Total: $" << register1.get_total() << endl;
 
 
 return 0;
 
-    
+
   }
